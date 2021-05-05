523e865e5b1a0fe5d533c60cdcdddcd079c26ca0
Ros McMahon <rosmcmahon@gmail.com>
2021-05-05 11:06:57 +0100

    logging: log to file

--- src/utils/logger.ts
+++ src/utils/logger.ts
@@ -1,7 +1,15 @@
 import col from 'ansi-colors'
+import { EOL } from 'os'
+
+let appendFile: any
+if(typeof window === 'undefined'){ 
+	appendFile = require('fs').appendFile 
+}
 
 let DEBUG_MESSAGES = true
 export const setDebugMessagesOn = (b: boolean) => DEBUG_MESSAGES = b
+let LOGFILE = true
+export const useLogfile = (b: boolean) => LOGFILE = b
 
 export const logger = (...args: any[]) => {
 	if(DEBUG_MESSAGES){
@@ -10,5 +18,12 @@ export const logger = (...args: any[]) => {
 		}else{
 			console.log('%c[arweave-uploader:]', 'color: #0000ff', ...args)
 		}
+	}
+	if(LOGFILE && typeof window === 'undefined'){
+		appendFile(
+			'aruploader.log', 
+			'\"' + new Date().toUTCString() + '\",' + args.join(',') + EOL,
+			()=>{}
+		)
 	}
 }
